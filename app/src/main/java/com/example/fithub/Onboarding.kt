0c916904907ad2f202577bb9723b6a9f73cdcd91
Kamil Knapik <knapik.kamil@outlook.com>
2025-09-01 13:56:14 +0200

    bmi calc simple

--- app/src/main/java/com/example/fithub/Onboarding.kt
+++ app/src/main/java/com/example/fithub/Onboarding.kt
@@ -1,6 +1,38 @@
 package com.example.fithub
 
-class Onboarding {
-    fun getInformation(age, gender, height, weight, activity_level, goal):
-            val age = age
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+class Onboarding : AppCompatActivity() {
+    private lateinit var name: EditText
+    private lateinit var weight: EditText
+    private lateinit var height: EditText
+    private lateinit var btnCalculate: Button
+    private lateinit var result: EditText
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.onboarding)
+
+        name = findViewById(R.id.etName)
+        weight = findViewById(R.id.etWeigth)
+        height = findViewById(R.id.etHeight)
+        btnCalculate = findViewById(R.id.btnCalculate)
+        result = findViewById(R.id.etResult)
+
+        btnCalculate.setOnClickListener {
+            val weightVal = weight.text.toString().toDouble()
+            val heightVal = height.text.toString().toDouble()
+            val bmi = calculateBMI(weightVal, heightVal)
+            result.setText(bmi.toString())
+        }
+    }
+
+    private fun calculateBMI(weight: Double, height: Double): Double {
+        val heightInMeters = height / 100
+        return weight / (heightInMeters * heightInMeters)
+    }
 }
